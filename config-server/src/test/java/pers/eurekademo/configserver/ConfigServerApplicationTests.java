7cb3d8c740f7dfe08e45ab4d1f6260e96b23109b
ccybqq <105545077+ccybqq@users.noreply.github.com>
2022-08-17 17:06:46 +0800

    Added cloud config.

---
+++ config-server/src/test/java/pers/eurekademo/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.eurekademo.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

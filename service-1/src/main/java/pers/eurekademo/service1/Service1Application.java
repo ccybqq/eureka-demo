8fdcfa46dba12b24a205b10dccb4c25e45850efe
ccybqq <105545077+ccybqq@users.noreply.github.com>
2022-08-17 17:33:20 +0800

    Initial state.

---
+++ service-1/src/main/java/pers/eurekademo/service1/Service1Application.java
@@ -0,0 +1,13 @@
+package pers.eurekademo.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+
+}

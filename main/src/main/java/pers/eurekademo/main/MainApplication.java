c64bb09da99c3d1f857bbc8a21ef5589d1742c64
ccybqq <105545077+ccybqq@users.noreply.github.com>
2022-08-17 16:52:21 +0800

    Main server up.

---
+++ main/src/main/java/pers/eurekademo/main/MainApplication.java
@@ -0,0 +1,15 @@
+package pers.eurekademo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}

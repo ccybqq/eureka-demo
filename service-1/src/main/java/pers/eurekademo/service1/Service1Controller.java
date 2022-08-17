0007356074dc2ef0f3021c17ca2aec2e9619dc0c
ccybqq <105545077+ccybqq@users.noreply.github.com>
2022-08-17 20:48:48 +0800

    Gateway and loadbalancing implemented.

--- service-1/src/main/java/pers/eurekademo/service1/Service1Controller.java
+++ service-1/src/main/java/pers/eurekademo/service1/Service1Controller.java
@@ -2,6 +2,7 @@ package pers.eurekademo.service1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,10 @@ public class Service1Controller {
     @PostMapping("/service1/save")
     public ResponseEntity<Service1Module> save(@RequestBody Service1Module service1Module) {
         return service1Service.save(service1Module);
+    }
+
+    @GetMapping("/service1/iam")
+    public String iam() {
+        return "I am Service 1.";
     }
 }
